75bb690f9d9639b6263459d221db5bff3a1c5509
sujeetk007 <141243432+sujeetk007@users.noreply.github.com>
2024-09-24 12:35:13 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 455324
+let accountEmail = "jeet0@gmail.com"
+var accountpassword = "12345"
+accountCity = "Patna"
+
+
+accountEmail = "jeet02@gmail.com"
+accountpassword = "224414"
+accountCity = "Gaya"
