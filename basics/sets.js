67f4141d16b75bbdc353c21caf604c0f81e9b39c
sujeetk007 <141243432+sujeetk007@users.noreply.github.com>
2024-09-27 12:06:32 +0000

    sets

---
+++ basics/sets.js
@@ -0,0 +1,8 @@
+let employees = new Set();
+employees.add("Ravi");
+employees.add("Divya");
+employees.add("Sony");
+employees.add("Priya");
+employees.add("Sonu Sehgal");
+
+console.log(employees);
