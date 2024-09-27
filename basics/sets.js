a06c4d9691af63c03e86c089411b874217090550
sujeetk007 <141243432+sujeetk007@users.noreply.github.com>
2024-09-27 12:13:10 +0000

    sets

--- basics/sets.js
+++ basics/sets.js
@@ -6,3 +6,19 @@ employees.add("Priya");
 employees.add("Sonu Sehgal");
 
 console.log(employees);
+
+
+if (employees.has("Sonu Sehgal")) {
+    console.log("Sonu Sehgal is included in the set.");
+  }
+  
+  if (employees.has("Krishna")) {
+    console.log("Krishna is included in the set.");
+  } else {
+    console.log("No Krishna Sorry");
+  }
+  
+  for (const employee of employees) {
+    console.log(employee);
+  }
+  
