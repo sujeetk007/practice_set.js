df5331d79829dbdf4f5538932fca10850bd43bb5
sujeetk007 <141243432+sujeetk007@users.noreply.github.com>
2025-03-08 16:57:47 +0000

    array declaration

--- basics/02_array.js
+++ basics/02_array.js
@@ -22,5 +22,13 @@ console.log(real_another_array);
 
 console.log(Array.isArray("Sujeet"))
 console.log(Array.from("Sujeet"))
+console.log(Array.from({name:"Sujeet"}))  // interesting
 
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+
+console.log(Array.of(score1, score2, score3));
 
